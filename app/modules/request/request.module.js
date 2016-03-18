eb28197fbe2d7e33be1d978b0eec08a896ca82b0
Vinícius do Vale <viniciusdovale10@gmail.com>
2016-03-18 12:41:25 -0300

    filters

--- app/modules/request/request.module.js
+++ app/modules/request/request.module.js
@@ -1,5 +1,35 @@
-angular.module('requestModule', [])  
-.controller('requestController', ['$scope', '$http',
-  function($scope, $http){
-  }
-]);
+angular.module('requestModule', ["isteven-multi-select"])  
+.controller('GithubController', GithubController);
+
+
+function GithubController($scope, $http) {
+	$scope.sizes = [ {name: 'The Red Chapel'}, {name: '2014'}];
+  
+  $scope.sendMessage = function () {
+  	$scope.data = [];
+  	console.log(JSON.stringify($scope.outputBrowsers, null, 4));
+  	//$scope.adress = document.getElementById("valueAdress").value;
+		console.log($scope.data.repeatSelect);
+
+		var REQ = {
+	    url: 'http://netflixroulette.net/api/api.php?&type=plain',
+	    method: 'GET'
+	  };
+	  $http(REQ)
+	    .success(function(data){
+	    	$scope.loading = false;
+	      $scope.data = data;
+
+
+	    })
+	    .error(function(err){
+	    	$scope.loading = false;
+	      console.log('Nenhum título encontrado');
+	    });
+  };    
+   
+  	
+  
+};
+
+GithubController.$inject = ['$scope', '$http'];
